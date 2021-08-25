bfa706014b119c751ebda2cf2e669f6cc4a06f40
carlos <carlosjmnz79@gmail.com>
2021-08-24 22:03:37 -0400

    step3: ok

--- prob_1.py
+++ prob_1.py
@@ -17,6 +17,7 @@ def walking_json(json_file_name='./data/data1.json'):
 
 	var1_sum = 0
 	var1_show_up = 0
+	var1_max = 0
 
 	var2_sum = 0
 
@@ -32,15 +33,21 @@ def walking_json(json_file_name='./data/data1.json'):
 						# print(f"\t\t\t{var} {value}")
 						var1_sum += int(value)
 						var1_show_up += 1
+						if region['name'] == 'Region4':
+							if int(value) > var1_max:
+								var1_max = int(value)
 					if province['name'] == 'Provincia2':
 						if var == 'var2':
 							var2_sum += int(value)
 							# print(f"\t\t\t{var2_sum}")
 
+
 	var1_average = float(var1_sum)/var1_show_up
 	# print(f"{var1_sum} {var1_show_up} {var1_average}")
 
 	print(f"{var2_sum}")
+
+	print(f"{var1_max}")
 
 	'''
 	# Print the type of data variable
