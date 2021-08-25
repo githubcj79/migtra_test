0a038fc85143fe7838304812d5d72849cb3249fb
carlos <carlosjmnz79@gmail.com>
2021-08-24 21:21:01 -0400

    step1: ok

--- prob_1.py
+++ prob_1.py
@@ -15,6 +15,9 @@ def walking_json(json_file_name='./data/data1.json'):
 	with open(json_file_name) as json_file:
 		_list = json.load(json_file)
 
+	var1_sum = 0
+	var1_show_up = 0
+
 	for region in _list:
 		print(f"{region['name']}")
 		for province in region['children']:
@@ -22,8 +25,14 @@ def walking_json(json_file_name='./data/data1.json'):
 			for city in province['children']:
 				print(f"\t\t{city['name']}")
 				for var, value in city['values'].items():
-					print(f"\t\t\t{var} {value}")
+					# print(f"\t\t\t{var} {value}")
+					if var == 'var1':
+						print(f"\t\t\t{var} {value}")
+						var1_sum += int(value)
+						var1_show_up += 1
 
+	var1_average = float(var1_sum)/var1_show_up
+	print(f"{var1_sum} {var1_show_up} {var1_average}")
 
 	'''
 	# Print the type of data variable
